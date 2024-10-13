558ae9bbde86e6103d8557af58cbfe85631988aa
Christopher Rybicki <crybicki98@gmail.com>
2024-10-13 02:21:23 -0400

    add TestBufferMgr

--- buffer_test.go
+++ buffer_test.go
@@ -53,3 +53,71 @@ func TestBuffer(t *testing.T) {
 	p2.SetInt(80, 9999)
 	b2.SetModified(1, 0)
 }
+
+func TestBufferMgr(t *testing.T) {
+	t.Cleanup(func() {
+		os.RemoveAll("buffermgrtest")
+	})
+
+	db, err := NewSimpleDB("buffermgrtest", 400, 3) // only 3 buffers
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	bm := db.BufferMgr
+
+	bs := make([]*Buffer, 6)
+
+	bs[0], err = bm.Pin(NewBlockId("testfile", 0))
+	if err != nil {
+		t.Fatal(err)
+	}
+	bs[1], err = bm.Pin(NewBlockId("testfile", 1))
+	if err != nil {
+		t.Fatal(err)
+	}
+	bs[2], err = bm.Pin(NewBlockId("testfile", 2))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	bm.Unpin(bs[1])
+	bs[1] = nil
+
+	bs[3], err = bm.Pin(NewBlockId("testfile", 0)) // block 0 pinned twice
+	if err != nil {
+		t.Fatal(err)
+	}
+	bs[4], err = bm.Pin(NewBlockId("testfile", 1)) // block 1 repinned
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("Available buffers: %d", bm.Available())
+
+	t.Log("Attempting to pin block 3...")
+	_, err = bm.Pin(NewBlockId("testfile", 3)) // will not work; no buffers left
+	if err == nil {
+		t.Fatal("Expected BufferAbortError, but got nil")
+	}
+	if _, ok := err.(*BufferAbortError); !ok {
+		t.Fatalf("Expected BufferAbortError, but got %v", err)
+	}
+	t.Log("Exception: No available buffers")
+
+	bm.Unpin(bs[2])
+	bs[2] = nil
+
+	bs[5], err = bm.Pin(NewBlockId("testfile", 3)) // now this works
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log("Final Buffer Allocation:")
+	for i, b := range bs {
+		if b != nil {
+			t.Logf("bs[%d] pinned to block %v", i, b.Blk)
+		}
+	}
+}
